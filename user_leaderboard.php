c31316b90a504828d980567570ba562b8920bef1
Mervin Ooi <mervinooi221@gmail.com>
2025-02-13 23:07:23 +0800

    1

--- user_leaderboard.php
+++ user_leaderboard.php
@@ -21,58 +21,8 @@ if ($conn->connect_error) {
 
 $username = $_SESSION['username'];
 
+include 'user_fetch_profile.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['update_username'])) {
-        $new_username = $_POST['newUsername'];
-
-        $stmt = $conn->prepare("UPDATE user SET Username = ? WHERE Username = ?");
-        $stmt->bind_param("ss", $new_username, $username);
-        if ($stmt->execute()) {
-            $_SESSION['username'] = $new_username;
-            $username = $new_username;
-        } else {
-            echo "Error updating username.";
-        }
-
-        $stmt->close();
-    }
-
-    if (isset($_POST['update_password'])) {
-        $new_password =$_POST['newPassword'];
-        $stmt = $conn->prepare("UPDATE user SET Password = ? WHERE Username = ?");
-        $stmt->bind_param("ss", $new_password, $username);
-        if ($stmt->execute()) {
-        } else {
-            echo "Error updating password.";
-        }
-        $stmt->close();
-    }
-
-    if (isset($_POST['update_profile'])) {
-        $profile_picture = $_FILES['ProfilePicture']['name'];
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($profile_picture);
-
-        if (move_uploaded_file($_FILES['ProfilePicture']['tmp_name'], $target_file)) {
-            $stmt = $conn->prepare("UPDATE user SET ProfilePicture = ? WHERE Username = ?");
-            $stmt->bind_param("ss", $target_file, $username);
-            if ($stmt->execute()) {
-                $profile_picture_path = $target_file;
-            } else {
-                echo "Error updating profile picture.";
-            }
-
-            $stmt->close();
-        } else {
-            echo "Error uploading file.";
-        }
-    }
-
-    // Redirect to avoid form resubmission
-    header("Location: user_mainPage.php");
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
@@ -83,7 +33,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Document</title>
     <link rel="stylesheet" href="user_header.css">
     <link rel="stylesheet" href="user_footer.css">
-    <link rel="stylesheet" href="user_hamburger_menu.css">
     <style>
         
         #content {
@@ -199,7 +148,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
     
-
+    <?php include 'user_hamburger_menu.php'; ?>
 
     <div id="content">
         <div id="leaderboard">
